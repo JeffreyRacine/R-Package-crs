a78e3825238d6c30c60f7b0035b8e643df23ad43
Jeffrey Racine <racinej@mcmaster.ca>
2011-06-23 10:42:43 -0400

    Typo in description

--- demo/radial_rgl.R
+++ demo/radial_rgl.R
@@ -1,5 +1,5 @@
 ## This illustration considers the `radial function' and plots the
-## results by construct a 3D real-time rendering plot using OpenGL.
+## results by constructing a 3D real-time rendering plot using OpenGL.
 
 require(crs)
 require(rgl)
