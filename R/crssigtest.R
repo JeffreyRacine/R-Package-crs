826d84529f7c3f7691f9d613a4210bd00abe304a
Jeffrey Racine <racinej@mcmaster.ca>
2011-08-26 09:55:09 -0400

    Updating functionality (added reorder option)

--- R/crssigtest.R
+++ R/crssigtest.R
@@ -1,16 +1,25 @@
+## (C) Jeffrey S. Racine (2011)
+
 ## This function performs an individual bootstrap test of significance
 ## for a crs regression model. If the index `i' is provided it
 ## computes the test for predictor `i', otherwise it computes the test
 ## for all predictors, one at a time.
 
-crssigtest <- function(model=NULL,index=NULL,boot.num=399) {
+## Two methods for imposing the null are implemented, a) a residual
+## bootstrap and b) a reording of the predictor being tested (in
+## place) that breaks any systematic relationship between the
+## predictor and outcome.
+
+crssigtest <- function(model=NULL,index=NULL,boot.num=399,boot.type=c("residual","reorder")) {
 
   if(is.null(model)) stop(" you must provide a crs model")
   if(is.null(index)) index <- 1:NCOL(model$xz)
   if(index < 1 || index > NCOL(model$xz)) stop(" you must provide a valid index")
   P.vec <- numeric(length(index))
   F.vec <- numeric(length(index))  
-  F.boot <- numeric(length=boot.num)  
+  F.boot <- numeric(length=boot.num)
+
+  boot.type <- match.arg(boot.type)
 
   for(i in 1:length(index)) {
 
@@ -24,8 +33,8 @@ crssigtest <- function(model=NULL,index=NULL,boot.num=399) {
 
     ## Placeholders for null degree/lambda
 
-    degree.null <- model.degree
-    lambda.null <- model.lambda
+    degree.restricted <- model.degree
+    lambda.restricted <- model.lambda
 
     ## Determine whether variable(s) is numeric or factor
 
@@ -38,55 +47,127 @@ crssigtest <- function(model=NULL,index=NULL,boot.num=399) {
     ## degree/lambda for the predictor being tested
 
     if(xz.numeric) {
+
       ## Predictor being tested is numeric, so need to determine
       ## degree index
+
       degree.index <- 0
       for(j in 1:index[i]) if(is.numeric(model$xz[,j])) degree.index <- degree.index + 1
+
       ## If the degree is zero (i.e. cross-validation has determined a
       ## variable is `irrelevant'), allow the model to be included
       ## using a degree 1 fit
+
       model.degree[degree.index] <- ifelse(model$degree[degree.index]==0,1,model$degree[degree.index])
+
       ## Set the null degree to zero
-      degree.null[degree.index] <- 0
+
+      degree.restricted[degree.index] <- 0
+
     } else {
+
       ## Predictor being tested is categorical, so need to determine
       ## lambda index
+
       lambda.index <- 0
       for(j in 1:index[i]) if(!is.numeric(model$xz[,j])) lambda.index <- lambda.index + 1
+
       ## If the bandwidth is one (i.e. cross-validation has determined
       ## a variable is `irrelevant'), allow the model to be included
       ## using the `frequency' fit (bandwidth of basically zero)
+
       model.lambda[lambda.index] <- ifelse(isTRUE(all.equal(model$lambda[lambda.index],1)),.Machine$double.eps,model$lambda[lambda.index])
+
       ## Set the null bandwidth to one
-      lambda.null[lambda.index] <- 1
+
+      lambda.restricted[lambda.index] <- 1
+
     }
 
-    model.unrestricted <- crs(xz=model$xz,y=model$y,cv="none",degree=model.degree,segments=model.segments,lambda=model.lambda,basis=model.basis,knots=model.knots)
-    model.null <- crs(xz=model$xz,y=model$y,cv="none",degree=degree.null,segments=model.segments,lambda=lambda.null,basis=model.basis,knots=model.knots)
+    model.unrestricted <- crs(xz=model$xz,
+                              y=model$y,
+                              cv="none",
+                              degree=model.degree,
+                              lambda=model.lambda,
+                              segments=model.segments,
+                              basis=model.basis,
+                              knots=model.knots)
+
+    model.restricted <- crs(xz=model$xz,
+                            y=model$y,
+                            cv="none",
+                            degree=degree.restricted,
+                            lambda=lambda.restricted,
+                            segments=model.segments,
+                            basis=model.basis,
+                            knots=model.knots)
 
     ## Compute the pseudo F-value under the null
 
     uss <- sum(residuals(model.unrestricted)^2)
-    rss <- sum(residuals(model.null)^2)
+    rss <- sum(residuals(model.restricted)^2)
   
     F.pseudo <- (rss-uss)/uss
     F.vec[i] <- F.pseudo
 
+    if(boot.type=="reorder") xz.boot <- model$xz
+
     for(b in 1:boot.num) {
     
-      ## Bootstrap sample under the null
+      ## Bootstrap sample under the null and recompute the
+      ## `restricted' and `unrestricted' models for the null sample
+
+      if(boot.type=="reorder") {
       
-      y.boot <- fitted(model.null) + sample(residuals(model.null),replace=T)
-      
-      ## Recompute the `restricted' and `unrestricted' models for the
-      ## null sample
-      
-      model.unrestricted.boot <- crs(xz=model$xz,y=y.boot,cv="none",degree=model.degree,segments=model.segments,lambda=model.lambda,basis=model.basis,knots=model.knots)
-      model.null.boot <- crs(xz=model$xz,y=y.boot,cv="none",degree=degree.null,segments=model.segments,lambda=lambda.null,basis=model.basis,knots=model.knots)
-      
+        xz.boot[,index[i]] <- sample(model$xz[,index[i]],replace=T)
+
+        model.unrestricted.boot <- crs(xz=xz.boot,
+                                       y=model$y,
+                                       cv="none",
+                                       degree=model.degree,
+                                       segments=model.segments,
+                                       lambda=model.lambda,
+                                       basis=model.basis,
+                                       knots=model.knots)
+
+        model.restricted.boot <- crs(xz=xz.boot,
+                                     y=model$y,
+                                     cv="none",
+                                     degree=degree.restricted,
+                                     segments=model.segments,
+                                     lambda=lambda.restricted,
+                                     basis=model.basis,
+                                     knots=model.knots)
+
+      } else {
+
+        y.boot <- fitted(model.restricted) + sample(residuals(model.restricted),replace=T)
+
+        model.unrestricted.boot <- crs(xz=model$xz,
+                                       y=y.boot,
+                                       cv="none",
+                                       degree=model.degree,
+                                       segments=model.segments,
+                                       lambda=model.lambda,
+                                       basis=model.basis,
+                                       knots=model.knots)
+
+        model.restricted.boot <- crs(xz=model$xz,
+                                     y=y.boot,
+                                     cv="none",
+                                     degree=degree.restricted,
+                                     segments=model.segments,
+                                     lambda=lambda.restricted,
+                                     basis=model.basis,
+                                     knots=model.knots)
+
+      }
+
+
       ## Recompute the pseudo F-value under the null
       
-      rss.boot <- sum(residuals(model.null.boot)^2)    
+      rss.boot <- sum(residuals(model.restricted.boot)^2)    
+      
       uss.boot <- sum(residuals(model.unrestricted.boot)^2)
       
       F.boot[b] <- (rss.boot-uss.boot)/uss.boot
@@ -97,6 +178,6 @@ crssigtest <- function(model=NULL,index=NULL,boot.num=399) {
 
   }
 
-  return(list(index=index,P=P.vec,F=F.vec))
+  return(list(index=index,P=P.vec,F=F.vec,F.boot=F.boot))
 
 } 
