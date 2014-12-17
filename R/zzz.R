57bef2a573e5da67ec1269909f71ada5e204322c
Bernd Bischl <bernd_bischl@gmx.net>
2014-12-18 00:02:08 +0100

    move option init from onAttach to onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,12 @@
 .onAttach <- function (lib, pkg) {
 	packageStartupMessage("Categorical Regression Splines (version 0.15-23)\n[vignette(\"crs_faq\") provides answers to frequently asked questions]", domain = NULL,  appendLF = TRUE)
+}
+
+.onLoad <- function (lib, pkg) {
   if(is.null(options('crs.messages')$crs.messages))
     options(crs.messages = TRUE)
-
 }
+
 .onUnload <- function (lpath){
   library.dynam.unload("crs", libpath=lpath)
 }
