18fc265545e37839a72849c96577e763b1e5bc56
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-07 16:08:38 -0500

    Fix(shellcheck): Address shellcheck warnings in mkcrspkg.sh and cleanup scripts

--- mkcrspkg.sh
+++ mkcrspkg.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-maver=`cat crsver`
+maver=$(cat crsver)
 miver=${maver##*-}
 maver=${maver%-*}
 
-if [ $1 ] 
+if [ "$1" ] 
     then 
-    let 'miver+=1' 
-    echo $maver-$miver > crsver
+    miver=$((miver+1)) 
+    echo "$maver"-"$miver" > crsver
 fi
 
 echo updating DESCRIPTION
-sed -i.bak -e 's/Version[:].*/'"Version: $maver-$miver/" -e 's/Date[:].*/'"Date: `date +%Y-%m-%d`/" DESCRIPTION
+sed -i.bak -e 's/Version[:].*/'"Version: $maver-$miver/" -e 's/Date[:].*/'"Date: $(date +%Y-%m-%d)/" DESCRIPTION
 
 rm DESCRIPTION.bak
 
-cd R
+(cd R || exit 1
 
 echo updating zzz.R
 sed -i.bak -e 's/version [0-9.][0-9.]*-[0-9][0-9]*/'"version $maver-$miver/" zzz.R 
@@ -24,4 +24,5 @@ rm zzz.R.bak
 
 cp spline.R ../demo
 
+) # end subshell
 cd ..
