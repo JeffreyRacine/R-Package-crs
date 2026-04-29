eb5e09dee79cdb89c96336cf00cec580405715d9
Jeffrey Racine <racinej@mcmaster.ca>
2026-04-29 12:33:37 -0400

    Point crs startup banner to gallery

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function (lib, pkg) {
   packageStartupMessage(
     sprintf(
-      'crs %s: vignette("crs_getting_started", package = "crs")',
+      'crs %s: examples and guides at https://jeffreyracine.github.io/gallery/; vignette("crs_getting_started", package = "crs")',
       utils::packageDescription(pkg, fields = "Version")
     ),
     domain = NULL,
